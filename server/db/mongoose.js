adf99a97aba21e0c085f08638e20ab930f963f4a
cinfinit <39793609+cinfinit@users.noreply.github.com>
2018-06-25 16:05:30 +0530

    initial

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose =require('mongoose');
 mongoose.Promise= global.Promise;
  // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
- mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds117681.mlab.com:17681/todo' || 'mongodb://localhost:27017/TodoApp');
+ mongoose.connect('mongodb://root:root123@ds117681.mlab.com:17681/todo' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports={mongoose};
